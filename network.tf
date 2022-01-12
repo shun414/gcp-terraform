03d3e6ecd493800998e3933642dc49ce35da0624
shun414 <shunshira14@gmail.com>
2022-01-12 19:23:27 +0900

    Add VPC network

---
+++ network.tf
@@ -0,0 +1,13 @@
+
+resource "google_compute_network" "this" {
+  name = var.gcp_project
+
+  auto_create_subnetworks = false
+}
+ 
+resource "google_compute_subnetwork" "subnet1" {
+  name          = "subnet-1"
+  ip_cidr_range = "192.168.10.0/24"
+  network       = google_compute_network.this.name
+  region        = var.region
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,6 @@
 variable "gcp_project" {}
 variable "billing_id" {}
+
+variable "region" {
+  default = "asia-northeast1"
+}
